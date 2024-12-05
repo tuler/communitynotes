96cba2333fd2b668c1220ba62f68911b10566c20
Danilo Tuler <tuler@pobox.com>
2024-12-05 10:06:03 -0500

    Script to download data

---
+++ scripts/download_data.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+DATE=2024/12/04
+BASE_URL=https://ton.twimg.com/birdwatch-public-data
+
+mkdir -p input/ratings
+
+# download notes file
+file=${BASE_URL}/${DATE}/notes/notes-00000.tsv
+echo "Downloading ${file}"
+curl -L --compressed ${file} -o input/notes-00000.tsv
+
+# download 16 ratings files
+for i in {0..15}; do
+    num=$(printf "%05d" $i)
+    file=${BASE_URL}/${DATE}/noteRatings/ratings-${num}.tsv
+    echo "Downloading ${file}"
+    curl -L --compressed ${file} -o input/ratings/ratings-${num}.tsv
+done
+
+#download other files
+file=${BASE_URL}/${DATE}/noteStatusHistory/noteStatusHistory-00000.tsv
+echo "Downloading ${file}"
+curl -L --compressed ${file} -o input/noteStatusHistory-00000.tsv
+
+file=${BASE_URL}/${DATE}/userEnrollment/userEnrollment-00000.tsv
+echo "Downloading ${file}"
+curl -L --compressed ${file} -o input/userEnrollment-00000.tsv
