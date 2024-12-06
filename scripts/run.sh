d3029f5c46c1624644f86d767f99692c9af1a1e5
Danilo Tuler <tuler@pobox.com>
2024-12-06 12:04:40 -0500

    Fix run script

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 docker run \
-    -v $(PWD)/input:/input:ro \
-    -v $(PWD)/output:/output \
+    -v $(pwd)/input:/input:ro \
+    -v $(pwd)/output:/output \
     communitynotes \
     -e /input/userEnrollment-00000.tsv \
     -n /input/notes-00000.tsv \
     -r /input/ratings \
     -s /input/noteStatusHistory-00000.tsv \
-    -o /output \
+    -o /output
